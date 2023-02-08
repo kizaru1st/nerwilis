71bd729aaa5eb72570d57deb3e6481ae28483134
Muhammad Reza Rizki Rahmadi <rezarizki77@gmail.com>
2023-02-08 09:57:37 +0700

    .

--- pages/4_🌍_RLS.py
+++ pages/4_🌍_RLS.py
@@ -1,10 +1,7 @@
 # Libraries
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import matplotlib.pyplot as plt
-import numpy as np
-import sys
 
 st.set_page_config(page_title='Dashboard Nerwillis',
                    page_icon=':bar_chart:', layout='wide')
