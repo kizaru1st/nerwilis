c1149aa2beb544ec8d80368588baca6d782e3f62
Muhammad Reza Rizki Rahmadi <rezarizki77@gmail.com>
2023-03-29 21:19:57 +0700

    fix cloud hosting

--- pages/1_🌍_IPM.py
+++ pages/1_🌍_IPM.py
@@ -2,8 +2,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objs as go
-import altair as alt
 
 st.set_page_config(page_title='Dashboard Nerwillis',
                    page_icon=':bar_chart:', layout='wide')
